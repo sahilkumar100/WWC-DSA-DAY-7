7a990a52411e94b89d32de36c34c75fdd8581b36
sahilkumar100 <120113586+sahilkumar100@users.noreply.github.com>
2024-12-28 23:12:15 +0530

    First commit

--- Triangle.cpp
+++ Triangle.cpp
@@ -66,27 +66,100 @@
 // }
 
 
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int i = 5;
+//     int num =1;
+
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<i; j++){
+//             cout<< " " ;
+//         }
+
+//         for(int j=0; j<n-i; j++){
+//             cout<< (i+1);
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+ 
+
+//  Q.>  1      i=0
+//      121     i=1
+//     12321    i=2
+//    1234321   i=3 
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n = 5;
+//     for(int i=0; i<n; i++){
+//         // for space 
+//         for(int j=0; j<n-i-1; j++){
+//             cout<< " ";
+//         }
+
+//         for(int j =1; j<=i+1; j++){
+//             cout<< j ;
+//         }
+//         for(int j=i; j>0; j--){
+//             cout<< j ;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+
+
 #include<iostream>
 using namespace std;
 
-int main(){
-    int i = 5;
-    int num =1;
+int main() {
+    int n = 5; // You can change 'n' for a bigger pattern
 
-    for(int i=0; i<n; i++){
-        for(int j=0; j<i; j++){
-            cout<< " " ;
+    // Upper half of the butterfly
+    for(int i = 1; i <= n; i++) {
+        // Left stars
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
         }
-
-        for(int j=0; j<n-i; j++){
-            cout<< (i+1);
+        // Spaces
+        for(int j = 1; j <= 2 * (n - i); j++) {
+            cout << " ";
+        }
+        // Right stars
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
         }
         cout << endl;
     }
+
+    // Lower half of the butterfly
+    for(int i = n; i >= 1; i--) {
+        // Left stars
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        // Spaces
+        for(int j = 1; j <= 2 * (n - i); j++) {
+            cout << " ";
+        }
+        // Right stars
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
- 
-
 
 
 
